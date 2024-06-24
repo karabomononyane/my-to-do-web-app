8c800e0e1269a10b17fb9abab28b7ef488e929e7
MonixDeep <mononyanekk@gmail.com>
2024-06-24 16:13:32 +0200

    Changed functions.py to include todos.txt path to be valid on server not locally

--- web_app1/functions.py
+++ web_app1/functions.py
@@ -1,16 +1,19 @@
-FILEPATH = 'todos.txt'
+import os
+
+import os
 
 
-def get_todos(filepath=FILEPATH):
+def get_todos(todos_local):
     """ Read a text file and return a list of
         to-do items
     """
+    filepath = os.path.join(os.getcwd(), "todos.txt")
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_args, filepath=FILEPATH):
+def write_todos(todos_args, filepath):
     """Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         todos = file.writelines(todos_args)
